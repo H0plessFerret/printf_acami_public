788b93bd14e7813da69009a30fdd915ff1cc8a62
Ardelle Cami <acami@su-d3.msk.21-school.ru>
2021-04-22 20:02:58 +0300

    Fixed more minor issues

--- srcs/ft_find_corresponding_print.c
+++ srcs/ft_find_corresponding_print.c
@@ -6,9 +6,14 @@
 /*   By: acami <acami@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/22 19:46:55 by acami             #+#    #+#             */
-/*   Updated: 2021/04/22 19:47:14 by acami            ###   ########.fr       */
+/*   Updated: 2021/04/22 20:00:06 by acami            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/ft_printf.h"
 #include "../headers/ft_print_type.h"
+
+int			(*ft_find_corresponding_print(uint32_t mask))(void *)
+{
+	
+}
